7ae8b8c4be67b7477d6a7195ede3bba630c0e90b
mariagajda <maria.e.gajda@gmail.com>
2021-11-07 13:22:19 +0100

    task_&_selectAnOption_without_fileUpdate

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,64 +1,74 @@
+import org.apache.commons.lang3.ArrayUtils;
 import pl.coderslab.ConsoleColors;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TaskManager {
 
     public static void main(String[] args) {
-        tasksDownload();
+        tasks();
         selectAnOption();
     }
 
-    public static void tasksDownload() {
+
+    //liczy linie z wczytywanego pliku:
+    public static int numberOflines() {
         File file = new File("tasks.csv");
         int linesCounter = 0;
 
-//liczy linie z wczytywanego pliku:
-        try (Scanner scan1 = new Scanner(file)) {
-            while (scan1.hasNextLine()) {
-                scan1.nextLine();
+        try (Scanner scan = new Scanner(file)) {
+            while (scan.hasNextLine()) {
                 linesCounter++;
+                scan.nextLine();
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(linesCounter);
 
-// liczy kolumny z wczytywanego pliku i tworzy tablice jednowymiarową z liniami wczytanymi z pliku:
-        String[] lines = new String[linesCounter];
-        try (Scanner scan2 = new Scanner(file)) {
-            while (scan2.hasNextLine()) {
-                for (int i = 0; i < linesCounter; i++) {
-                    lines[i] = scan2.nextLine();
-                }
+        return linesCounter;
+    }
+
+
+    // liczy kolumny z wczytywanego pliku i tworzy tablice jednowymiarową z liniami wczytanymi z pliku:
+    public static String[] tableWithLines() {
+        File file = new File("tasks.csv");
+        String[] lines = new String[numberOflines()];
+        try (Scanner scan = new Scanner(file)) {
+            for (int i = 0; i < lines.length; i++) {
+                lines[i] = scan.nextLine();
             }
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
+        return lines;
+    }
+
+    //liczy kolumny:
+    public static int columnsCounter() {
         int columnsCounter = 0;
-        for (int i = 0; i < linesCounter; i++) {
-            String[] columns = lines[i].split(", ");
+        for (int i = 0; i < numberOflines(); i++) {
+            String[] columns = tableWithLines()[i].split(", ");
             columnsCounter = columns.length;
         }
-        System.out.println(columnsCounter);
+        return columnsCounter;
+    }
 
-// wczytuje dane do tablicy dwuwymiarowej:
-        String[][] tasks = new String[linesCounter][columnsCounter];
-        try (Scanner scan2 = new Scanner(file)) {
-            for (int i = 0; i < tasks.length; i++) {
-                String[] columns = lines[i].split(", ");
-                for (int j = 0; j < columns.length; j++) {
-                    tasks[i][j] = columns[j];
-                    System.out.println(tasks[i][j]);
-                }
+    public static String[][] tasks() {
+// wczytuje dane do tablicy dwuwymiarowej
+        String[][] tasks = new String[tableWithLines().length][columnsCounter()];
+        for (int i = 0; i < tableWithLines().length; i++) {
+            String[] columns = tableWithLines()[i].split(", ");
+            for (int j = 0; j < columnsCounter(); j++) {
+                tasks[i][j] = columns[j];
             }
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
         }
-
+        return tasks;
     }
 
 
@@ -70,7 +80,63 @@ public class TaskManager {
         }
         Scanner scan = new Scanner(System.in);
         String optionChosen = scan.nextLine();
-        System.out.println(optionChosen);
-    }
+        String[][] newTasks;
 
+        while (!optionChosen.equals("quit")) {
+            if (optionChosen.equals("add")) {
+                Scanner scan1 = new Scanner(System.in);
+                newTasks = new String[numberOflines() + 1][columnsCounter()];
+
+                System.out.println("Please add task description:");
+                String description = scan1.next();
+                System.out.println("Please add task due date");
+                String dueDate = scan1.next();
+                System.out.println("Is your task important: true/false");
+                String importance = scan1.next();
+
+                for (int i = 0; i < newTasks.length; i++) {
+                    if (i < newTasks.length - 1) {
+                        for (int j = 0; j < columnsCounter(); j++) {
+                            newTasks[i][j] = tasks()[i][j];
+                        }
+                    } else if (i == newTasks.length - 1) {
+                        newTasks[i][0] = description;
+                        newTasks[i][1] = dueDate;
+                        newTasks[i][2] = importance;
+                    }
+                }/*
+                try (FileWriter fileWriter = new FileWriter("task.csv", true)) {
+                    fileWriter.append("\n" + description + ", " + dueDate + ", " + importance);
+                } catch (IOException e) {
+                    System.out.println("File cannot be updated");
+                }*/
+                selectAnOption();
+            }
+            if (optionChosen.equals("remove")) {
+                Scanner scan2 = new Scanner(System.in);
+                System.out.println("Please select number to remove");
+                int num = scan2.nextInt();
+                newTasks = ArrayUtils.remove(tasks(), num);
+                System.out.println(Arrays.toString(newTasks));
+                selectAnOption();
+            }
+            if (optionChosen.equals("list")) {
+                System.out.println("List of tasks:");
+                for (int i = 0; i < tasks().length; i++) {
+                    System.out.print(i + " : ");
+                    for (int j = 0; j < columnsCounter(); j++) {
+                        System.out.print(tasks()[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+                selectAnOption();
+            }
+
+            if (optionChosen.equals("exit")) {
+                System.out.println(ConsoleColors.RED + "Bye, bye");
+            }
+        }
+
+    }
 }
+
